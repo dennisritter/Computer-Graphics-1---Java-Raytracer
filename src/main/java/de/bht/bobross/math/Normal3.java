d2dcb305765602b11fb2ef4d0fd9aa3fb8b4ada6
Jannik Portz <hello@jannikportz.de>
2015-11-01 15:15:22 +0100

    generated equals(), hashCode() and toString() for classes Normal3 and Point3

--- src/main/java/de/bht/bobross/math/Normal3.java
+++ src/main/java/de/bht/bobross/math/Normal3.java
@@ -57,4 +57,38 @@ public class Normal3 {
     return x * v.x + y * v.y + z * v.z;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Normal3 normal3 = (Normal3) o;
+
+    if (Double.compare(normal3.x, x) != 0) return false;
+    if (Double.compare(normal3.y, y) != 0) return false;
+    return Double.compare(normal3.z, z) == 0;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result;
+    long temp;
+    temp = Double.doubleToLongBits(x);
+    result = (int) (temp ^ (temp >>> 32));
+    temp = Double.doubleToLongBits(y);
+    result = 31 * result + (int) (temp ^ (temp >>> 32));
+    temp = Double.doubleToLongBits(z);
+    result = 31 * result + (int) (temp ^ (temp >>> 32));
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Normal3{" +
+        "x=" + x +
+        ", y=" + y +
+        ", z=" + z +
+        '}';
+  }
 }
