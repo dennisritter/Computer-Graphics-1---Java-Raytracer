949fe184b85e5da3d80b1815282876a01c27c99b
Jannik Portz <hello@jannikportz.de>
2015-11-03 17:03:36 +0100

    moved ImageFileFilter to ChooseImageFrame as inner class minor improvements in ChooseImageFrame

--- src/main/java/de/bht/bobross/image/ChooseImageFrame.java
+++ src/main/java/de/bht/bobross/image/ChooseImageFrame.java
@@ -2,52 +2,69 @@ package de.bht.bobross.image;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.Container;
 import java.awt.BorderLayout;
 import java.awt.Image;
+import java.io.File;
 import java.io.IOException;
 
 /**
  * Creates a new ChooseImageFrame and reads the selected image.
  *
- * @author      nathalie
-*/
+ * @author      Nathalie Junker
+ */
 public class ChooseImageFrame extends JFrame{
 
-  /** The container*/
-  final Container container;
-
-  /** The file dialog */
-  final JFileChooser chooser;
-
-  /** The file dialog's filter */
-  final ImageFileFilter filter;
-
-  /** The image to be displayed */
-  Image img;
-
-
-  public ChooseImageFrame(){
-    container = this.getContentPane();
-    chooser = new JFileChooser();
-    filter = new ImageFileFilter();
+  /**
+   * Constructs a new ChooseImageFrame and automatically displays a JFileChooser.
+   * When a file has been selected, it loads the image into the content container.
+   */
+  public ChooseImageFrame () {
+    final Container container = this.getContentPane();
+    final JFileChooser chooser = new JFileChooser();
 
     container.setLayout(new BorderLayout());
 
-    chooser.setFileFilter(filter);
-    chooser.showOpenDialog(null);
+    chooser.setFileFilter( new ImageFileFilter() );
+    chooser.showOpenDialog( null );
     try {
-      img = ImageIO.read(chooser.getSelectedFile());
+      final Image img = ImageIO.read(chooser.getSelectedFile());
+      displayImage(img);
     } catch (IOException ex){
       ex.printStackTrace();
     }
-    displayImage(img);
   }
 
-  /** Reads the selected file, stores it in variable img and adds it to the ImagePanel. */
-  public void displayImage(Image img){
+  /**
+   * Reads the selected file, stores it in variable img and adds it to the ImagePanel.
+   *
+   * @param     img       The image to display in the frame
+   */
+  protected void displayImage ( final Image img ) {
     final ImagePanel panel = new ImagePanel(img);
-    container.add(panel);
-    this.setSize(panel.getImgSize(img));
+    getContentPane().add(panel);
+    setSize(panel.getImgSize(img));
+  }
+
+  /**
+   * FileFilter for a JFileChooser, which accepts JPG and PNG files and directories.
+   */
+  public class ImageFileFilter extends FileFilter {
+
+    /**
+     * Accept file with .png or .jpg extensions and directories
+     *
+     * @param     f       The file to be checked.
+     */
+    public boolean accept ( final File f ) {
+      final String name = f.getName();
+      return f.isDirectory() || name.endsWith(".jpg") || name.endsWith(".png") || name.endsWith(".JPG") || name.endsWith(".PNG");
+    }
+
+    @Override
+    public String getDescription() {
+      return "JPG and PNG";
+    }
   }
 }
