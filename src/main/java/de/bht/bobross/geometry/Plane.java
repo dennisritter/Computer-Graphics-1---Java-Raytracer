3a5bedbe04a3a084d26fa729bc59a4202ec0f408
reknui <nathaliejunker@googlemail.com>
2015-11-10 19:14:44 +0100

    Plane added

--- src/main/java/de/bht/bobross/geometry/Plane.java
+++ src/main/java/de/bht/bobross/geometry/Plane.java
@@ -1,14 +1,33 @@
 package de.bht.bobross.geometry;
 
+import de.bht.bobross.Color;
 import de.bht.bobross.Ray;
+import de.bht.bobross.math.Normal3;
+import de.bht.bobross.math.Point3;
 
 /**
  * @author      Nathalie Junker
  */
 public class Plane extends Geometry {
 
+  public final Point3 a;
+  public final Normal3 n;
+
+
+  public Plane(final Point3 a, final Normal3 n, final Color color){
+    super(color);
+    this.a = a;
+    this.n = n;
+  }
+
   @Override
   public Hit hit(Ray r) {
-    return null;
+
+    double t = n.dot(a.sub(r.o))/n.dot(r.d);
+    if(t == 0){
+      return null;
+    }
+
+    return new Hit(t, r, this);
   }
 }
