6b903d5a65fbf05ee46dfb0b617f64d4de1aa1eb
reknui <nathaliejunker@googlemail.com>
2015-11-10 18:38:30 +0100

    renamed variables

--- src/main/java/de/bht/bobross/camera/OrthographicCamera.java
+++ src/main/java/de/bht/bobross/camera/OrthographicCamera.java
@@ -23,10 +23,10 @@ public class OrthographicCamera extends Camera {
     final Vector3 d = this.w.mul(-1);
     final int a = w / h;
 
-    int klammereins = (x-(w-1)/2)/(w-1);
-    int klammerzwei = (y-(h-1)/2)/(h-1);
-    double produkt1 = klammereins*s*a;
-    double produkt2 = klammerzwei*s;
+    double doub1 = (x-(w-1)/2)/(w-1);
+    double doub2 = (y-(h-1)/2)/(h-1);
+    double produkt1 = doub1*s*a;
+    double produkt2 = doub2*s;
 
     Vector3 vec1 = u.mul(produkt1);
     Vector3 vec2 = v.mul(produkt2);
