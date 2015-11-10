fe47f5a1feb77f6b6ca05c35fa49dae2489943d4
reknui <nathaliejunker@googlemail.com>
2015-11-10 19:00:58 +0100

    Plane added

---
+++ src/main/java/de/bht/bobross/geometry/Plane.java
@@ -0,0 +1,14 @@
+package de.bht.bobross.geometry;
+
+import de.bht.bobross.Ray;
+
+/**
+ * @author      Nathalie Junker
+ */
+public class Plane extends Geometry {
+
+  @Override
+  public Hit hit(Ray r) {
+    return null;
+  }
+}
