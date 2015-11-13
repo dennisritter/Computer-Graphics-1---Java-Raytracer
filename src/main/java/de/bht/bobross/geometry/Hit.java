85cfb53d0e6c418e508d469d89c7239af02ded49
Jannik Portz <hello@jannikportz.de>
2015-11-13 21:20:39 +0100

    implemented equals, hashCode and toString in all geometry classes

--- src/main/java/de/bht/bobross/geometry/Hit.java
+++ src/main/java/de/bht/bobross/geometry/Hit.java
@@ -35,5 +35,36 @@ public class Hit {
     this.geo = geo;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
 
+    Hit hit = (Hit) o;
+
+    if (Double.compare(hit.t, t) != 0) return false;
+    if (!ray.equals(hit.ray)) return false;
+    return geo.equals(hit.geo);
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result;
+    long temp;
+    temp = Double.doubleToLongBits(t);
+    result = (int) (temp ^ (temp >>> 32));
+    result = 31 * result + ray.hashCode();
+    result = 31 * result + geo.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Hit{" +
+        "t=" + t +
+        ", ray=" + ray +
+        ", geo=" + geo +
+        '}';
+  }
 }
