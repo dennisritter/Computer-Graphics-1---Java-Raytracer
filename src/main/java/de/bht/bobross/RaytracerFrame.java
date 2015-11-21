d591b6ef78887d8775740fdf53dd0541fe402a4f
reknui <nathaliejunker@googlemail.com>
2015-11-21 17:53:12 +0100

    added abgabekriterien-methods

--- src/main/java/de/bht/bobross/RaytracerFrame.java
+++ src/main/java/de/bht/bobross/RaytracerFrame.java
@@ -2,8 +2,8 @@ package de.bht.bobross;
 
 import de.bht.bobross.camera.Camera;
 import de.bht.bobross.camera.PerspectiveCamera;
-import de.bht.bobross.geometry.Geometry;
-import de.bht.bobross.geometry.Triangle;
+import de.bht.bobross.geometry.*;
+import de.bht.bobross.math.Normal3;
 import de.bht.bobross.math.Point3;
 import de.bht.bobross.math.Vector3;
 
@@ -22,7 +22,7 @@ public class RaytracerFrame extends JFrame {
   public RaytracerFrame(Raytracer raytracer) {
     this.raytracer = raytracer;
 
-    setSize( raytracer.image.getWidth(), raytracer.image.getHeight() );
+    setSize(raytracer.image.getWidth(), raytracer.image.getHeight());
     setLayout(new BorderLayout());
     add(new RaytracerCanvas(), BorderLayout.CENTER);
     setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
@@ -33,8 +33,48 @@ public class RaytracerFrame extends JFrame {
   }
 
   public static void main ( String[] args ) {
+    showSpheresSmallBig();
+  }
+
+  public static void showPlane(){
+    final World w = new World( new Geometry[] {
+        new Plane( new Point3(0, -1, 0), new Normal3(0, 1, 0), new Color(0, 1, 0) )
+    }, new Color(0, 0, 0) );
+    final Camera c = new PerspectiveCamera( new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4 );
+    final Frame frame = new RaytracerFrame( 640, 480, w, c );
+    frame.setVisible( true );
+  }
+
+  public static void showSphere(){
+    final World w = new World( new Geometry[] {
+        new Sphere( new Point3(0,0,-3), 0.5, new Color(1, 0, 0) )
+    }, new Color(0, 0, 0) );
+    final Camera c = new PerspectiveCamera( new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4 );
+    final Frame frame = new RaytracerFrame( 640, 480, w, c );
+    frame.setVisible( true );
+  }
+
+  public static void showBox(){
+    final World w = new World( new Geometry[] {
+        new AxisAlignedBox( new Point3(-0.5, 0, -0.5), new Point3(0.5, 1, 0.5), new Color(0, 0, 1) )
+    }, new Color(0, 0, 0) );
+    final Camera c = new PerspectiveCamera( new Point3(3, 3, 3), new Vector3(-3, -3, -3), new Vector3(0, 1, 0), Math.PI / 4 );
+    final Frame frame = new RaytracerFrame( 640, 480, w, c );
+    frame.setVisible( true );
+  }
+
+  public static void showTriangle(){
     final World w = new World( new Geometry[] {
         new Triangle( new Point3(-.5, .5, -3), new Point3(.5, .5, -3), new Point3(.5, -.5, -3), new Color(1, 0, 1) )
+    }, new Color(0, 0, 0) );
+    final Camera c = new PerspectiveCamera( new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4 );
+    final Frame frame = new RaytracerFrame( 640, 480, w, c );
+    frame.setVisible( true );
+  }
+
+  public static void showSpheresSmallBig(){
+    final World w = new World( new Geometry[] {
+        new Sphere( new Point3(-1, 0, -3), 0.5, new Color(1, 0, 0) ), new Sphere( new Point3(1, 0, -6), 0.5, new Color(1, 0, 0) )
     }, new Color(0, 0, 0) );
     final Camera c = new PerspectiveCamera( new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4 );
     final Frame frame = new RaytracerFrame( 640, 480, w, c );
@@ -50,4 +90,5 @@ public class RaytracerFrame extends JFrame {
       g.drawImage( raytracer.image, 0, 0, this );
     }
   }
+
 }
