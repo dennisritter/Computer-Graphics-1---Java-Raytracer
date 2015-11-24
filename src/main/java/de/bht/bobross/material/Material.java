09095774c73b00fb28a60b66ea6612febb120f0e
Jannik Portz <hello@jannikportz.de>
2015-11-24 17:39:50 +0100

    documented class Material added class SingleColorMaterial

--- src/main/java/de/bht/bobross/material/Material.java
+++ src/main/java/de/bht/bobross/material/Material.java
@@ -4,8 +4,21 @@ import de.bht.bobross.Color;
 import de.bht.bobross.World;
 import de.bht.bobross.geometry.Hit;
 
+/**
+ * Abstraction of a material covering a geometry
+ *
+ * @author      Jannik Portz
+ */
 abstract public class Material {
 
+  /**
+   * Determines the color of the geometry on a specific hit.
+   * Also takes the world's lights in consideration.
+   *
+   * @param     hit       The hit
+   * @param     world     The world containing all lights
+   * @return              The color of the geometry on the specified hit
+   */
   abstract public Color colorFor ( final Hit hit, final World world );
 
 }
