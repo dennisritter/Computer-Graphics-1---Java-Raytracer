f0ab34b98c3fd2ab43e786226b017421a0c2c6f1
Jannik Portz <hello@jannikportz.de>
2015-12-02 16:48:30 +0100

    added class Disc

--- src/main/java/de/bht/bobross/raytracer/RaytracerTest.java
+++ src/main/java/de/bht/bobross/raytracer/RaytracerTest.java
@@ -5,6 +5,7 @@ import de.bht.bobross.World;
 import de.bht.bobross.camera.Camera;
 import de.bht.bobross.camera.PerspectiveCamera;
 import de.bht.bobross.geometry.AxisAlignedBox;
+import de.bht.bobross.geometry.Disc;
 import de.bht.bobross.geometry.Geometry;
 import de.bht.bobross.geometry.Plane;
 import de.bht.bobross.geometry.Sphere;
@@ -40,16 +41,13 @@ public class RaytracerTest {
   static final SpotLight SPOTLIGHT = new SpotLight(WHITE, CAM_POSITION, CAM_DIRECTION, Math.PI/14);
 
   public static void main ( final String[] args ) {
-
-//    singleColor();
-//    pointLightLambert();
-//    pointLightPhong();
-//    dirLightPhong();
-//    spotLightPhong();
-//
-//    spotLightLambertAmbient25();
-//    spotLightLambert();
-//    spotLightSingleColor();
+    singleColor();
+    pointLightLambert();
+    pointLightPhong();
+    dirLightPhong();
+    spotLightPhong();
+    spotLightPhongAmbient();
+    pointLightPhongDisc();
   }
 
   public static void singleColor(){
@@ -97,6 +95,21 @@ public class RaytracerTest {
     frame.drawImage();
   }
 
+  public static void pointLightPhongDisc(){
+
+    final AxisAlignedBox aab = new AxisAlignedBox( new Point3(-1.5, 0.5, 0.5), new Point3(-0.5, 1.5, 1.5), new PhongMaterial(BLUE, WHITE, 64) );
+    final Plane plane =  new Plane( new Point3(0, 0, 0), new Normal3(0, 1, 0), new PhongMaterial(RED, WHITE, 64));
+    final Sphere sphere = new Sphere( new Point3(1, 1, 1), 0.5, new PhongMaterial(GREEN, WHITE, 64) );
+    final Disc disc = new Disc( new Point3(0,2,-1), new Normal3( 0, 0, 1 ), 0.5, new LambertMaterial( GREEN ) );
+
+    final Camera c = new PerspectiveCamera( CAM_POSITION, new Vector3(-1,-1,-1), new Vector3(0,1,0), Math.PI/4);
+    final World w = new World( new Geometry[] {aab, plane, sphere, disc}, new Light[] { POINTLIGHT }, BLACK, BLACK);
+
+    final RaytracerFrame frame = createRaytracerFrame(w, c, WIDTH, HEIGHT);
+    frame.setVisible(true);
+    frame.drawImage();
+  }
+
   public static void dirLightPhong(){
 
     final AxisAlignedBox aab = new AxisAlignedBox( new Point3(-1.5, 0.5, 0.5), new Point3(-0.5, 1.5, 1.5), new PhongMaterial(BLUE, WHITE, 64) );
@@ -121,6 +134,21 @@ public class RaytracerTest {
 
     final Camera c = new PerspectiveCamera( CAM_POSITION, new Vector3(-1,-1,-1), new Vector3(0,1,0), Math.PI/4);
     final World w = new World( new Geometry[] {aab, plane, sphere, triangle}, new Light[] { SPOTLIGHT }, BLACK, BLACK);
+
+    final RaytracerFrame frame = createRaytracerFrame(w, c, WIDTH, HEIGHT);
+    frame.setVisible(true);
+    frame.drawImage();
+  }
+
+  public static void spotLightPhongAmbient () {
+
+    final AxisAlignedBox aab = new AxisAlignedBox( new Point3(-1.5, 0.5, 0.5), new Point3(-0.5, 1.5, 1.5), new PhongMaterial(BLUE, WHITE, 64) );
+    final Plane plane =  new Plane( new Point3(0, 0, 0), new Normal3(0, 1, 0), new PhongMaterial(RED, WHITE, 64));
+    final Sphere sphere = new Sphere( new Point3(1,1,1), 0.5, new PhongMaterial(GREEN, WHITE, 64) );
+    final Triangle triangle = new Triangle( new Point3(0,0,-1), new Point3(1,0,-1), new Point3(1,1,-1), new PhongMaterial(YELLOW, WHITE, 64) );
+
+    final Camera c = new PerspectiveCamera( CAM_POSITION, new Vector3(-1,-1,-1), new Vector3(0,1,0), Math.PI/4);
+    final World w = new World( new Geometry[] {aab, plane, sphere, triangle}, new Light[] { SPOTLIGHT }, BLACK, new Color(.25, .25, .25));
 
     final RaytracerFrame frame = createRaytracerFrame(w, c, WIDTH, HEIGHT);
     frame.setVisible(true);
