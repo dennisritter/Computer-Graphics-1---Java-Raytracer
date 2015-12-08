330d7410ce7cfc3a54648a13d78246d30e73c619
reknui <nathaliejunker@googlemail.com>
2015-12-08 17:19:47 +0100

    implemented reflective material

--- src/main/java/de/bht/bobross/material/ReflectiveMaterial.java
+++ src/main/java/de/bht/bobross/material/ReflectiveMaterial.java
@@ -1,8 +1,13 @@
 package de.bht.bobross.material;
 
 import de.bht.bobross.Color;
+import de.bht.bobross.Ray;
 import de.bht.bobross.World;
 import de.bht.bobross.geometry.Hit;
+import de.bht.bobross.light.Light;
+import de.bht.bobross.math.Point3;
+import de.bht.bobross.math.Vector3;
+import de.bht.bobross.raytracer.Tracer;
 
 /**
  * Represents the reflective material.
@@ -31,7 +36,22 @@ public class ReflectiveMaterial extends Material{
   }
 
   @Override
-  public Color colorFor(Hit hit, World world) {
-    return null;
+  public Color colorFor(Hit hit, World world, Tracer tracer) {
+    Color c = diffuse.mul( world.ambientLightColor );
+    final Point3 p = hit.getPoint();
+
+    for (Light light : world.lights){
+      final Vector3 l = light.directionFrom(p);
+      final Vector3 r = l.reflectedOn(hit.normal); //
+      final Vector3 e = hit.ray.d.mul(-1);
+
+      if(light.illuminates(p, world)) {
+        final Color cTemp = diffuse.mul(light.color).mul(Math.max(0, (hit.normal.dot(l))))
+            .add(specular.mul(light.color).mul(Math.max(0, Math.pow(e.dot(r), exponent))));
+        final Color cReflection = reflection.mul(tracer.traceRay(new Ray(p, hit.ray.d.reflectedOn(hit.normal))));
+        c.add(cTemp).add(cReflection);
+      }
+    }
+    return c.limitComponents();
   }
 }
