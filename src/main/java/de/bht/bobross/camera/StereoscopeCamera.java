8e8aca5f50c3e4bc9469c5cd723c9d3ede4fde7d
Jannik Portz <hello@jannikportz.de>
2016-02-09 11:04:42 +0100

    added some doc to stereoscope cameras

--- src/main/java/de/bht/bobross/camera/StereoscopeCamera.java
+++ src/main/java/de/bht/bobross/camera/StereoscopeCamera.java
@@ -5,11 +5,28 @@ import de.bht.bobross.math.Point2;
 import de.bht.bobross.math.Point3;
 import de.bht.bobross.math.Vector3;
 
+/**
+ * Implementation for a Camera that renders two different images from two cameras on each side of the frame
+ * @author      Jannik Portz
+ */
 public class StereoscopeCamera extends Camera {
 
+  /** The camera for the left partial image */
   public final PerspectiveCamera leftCam;
+
+  /** The camera fpr the right partial image */
   public final PerspectiveCamera rightCam;
 
+  /**
+   * Constructs a new stereoscope camera with all attributes
+   *
+   * @param     e         The camera's eye-position
+   * @param     g         The camera's gaze direction
+   * @param     t         The camera's up-vector
+   * @param     angle     The angle for the cameras
+   * @param     distance  The distance between the two cameras
+   * @param     pattern   The sampling pattern to use
+   */
   public StereoscopeCamera ( final Point3 e, final Vector3 g, final Vector3 t, final double angle, final double distance, final SamplingPattern pattern ) {
     super( e, g, t, pattern );
 
--- src/main/java/de/bht/bobross/camera/StereoscopeSingleCamera.java
+++ src/main/java/de/bht/bobross/camera/StereoscopeSingleCamera.java
@@ -5,8 +5,22 @@ import de.bht.bobross.math.Point2;
 import de.bht.bobross.math.Point3;
 import de.bht.bobross.math.Vector3;
 
+/**
+ * Implementation of a camera that renders two partial images of two cameras in one image
+ * @author      Jannik Portz
+ */
 public class StereoscopeSingleCamera extends StereoscopeCamera {
 
+  /**
+   * Constructs a new stereoscope single camera with all attributes
+   *
+   * @param     e         The camera's eye-position
+   * @param     g         The camera's gaze direction
+   * @param     t         The camera's up-vector
+   * @param     angle     The angle for the cameras
+   * @param     distance  The distance between the two cameras
+   * @param     pattern   The sampling pattern to use
+   */
   public StereoscopeSingleCamera( final Point3 e, final Vector3 g, final Vector3 t, final double angle, final double distance, final SamplingPattern pattern) {
     super( e, g, t, angle, distance, pattern );
   }
